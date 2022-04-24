c1029a2ec6da51d4e63a17cb5f29b3fdc5d48b33
Moon <msm2230@naver.com>
2022-04-25 02:54:20 +0900

    로그인 화면전환 및 First responder 설정

--- MoonChat/LoginViewController.swift
+++ MoonChat/LoginViewController.swift
@@ -2,27 +2,64 @@ import UIKit
 import Firebase
 import TextFieldEffects
 
-class LoginViewController: UIViewController {
-    @IBOutlet var email: HoshiTextField!
-    @IBOutlet var password: HoshiTextField!
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var email: UITextField!
+    @IBOutlet var password: UITextField!
     @IBOutlet var signInButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     let remoteconfig = RemoteConfig.remoteConfig()
     var color: String!
     
     override func viewDidLoad() {
+        email.delegate = self
+        password.delegate = self
+        
+        try! Auth.auth().signOut()
         color = remoteconfig["splash_background"].stringValue
         
         signInButton.backgroundColor = UIColor(rgb: Int(color!) ?? 123).withAlphaComponent(1.0)
         signUpButton.backgroundColor = UIColor(rgb: Int(color!) ?? 123).withAlphaComponent(1.0)
         
         signUpButton.addTarget(self, action: #selector(presentSignup), for: .touchUpInside)
+        signInButton.addTarget(self, action: #selector(loginEvent), for: .touchUpInside)
+        
+        email.becomeFirstResponder()
+        
+        
+        Auth.auth().addStateDidChangeListener{ (auth, user) in
+            if user != nil {
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "MainViewController") as! MainViewController
+                
+                self.present(vc, animated: true, completion: nil)
+            }
+        }
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case email:
+            password.becomeFirstResponder()
+        case password:
+            password.resignFirstResponder()
+        default:
+            break
+        }
+        
+        return true
+    }
     
     @objc func presentSignup() {
         let signupVC = self.storyboard?.instantiateViewController(withIdentifier: "SignupViewController") as! SignupViewController
         self.present(signupVC, animated: true, completion: nil)
+    }
+    
+    @objc func loginEvent(){
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!, completion: { (user, err) in
+            if let err = err {
+                print("login error : \(err)")
+            }
+            
+        })
     }
     
 }
